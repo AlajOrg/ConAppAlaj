89691c169601c720fdd0b5c60bf8c69fbf8625f0
S07C09 <S07C09@S07C09>
2023-05-28 09:32:15 +0430

    get data from user

--- ConAppAlaj/Program.cs
+++ ConAppAlaj/Program.cs
@@ -1,7 +1,7 @@
 
 int sumnum(int a, int b)
 {
-    //a = int.Parse(Console.ReadLine());
-    //b = int.Parse(Console.ReadLine());
+    a = int.Parse(Console.ReadLine());
+    b = int.Parse(Console.ReadLine());
     return a + b;
 }
